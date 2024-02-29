591f5bb15bf133aaf8f3e021b76db4522ff4b962
Rhys Kim <lev.k.kim@gmail.com>
2024-02-29 15:32:35 -0500

    switch to vercel and pray

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,30 +2,25 @@ import { defineConfig } from 'astro/config';
 import { imageService } from '@unpic/astro/service';
 import node from "@astrojs/node";
 import icon from "astro-icon";
-
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  integrations: [
-    icon({
-      iconDir: "src/assets/icons"
-    }),
-    react(),
-    markdoc(),
-    ...process.env.SKIP_KEYSTATIC ? [] : [keystatic()],
-  ],
+  integrations: [icon({
+    iconDir: "src/assets/icons"
+  }), react(), markdoc(), ...(process.env.SKIP_KEYSTATIC ? [] : [keystatic()])],
   image: {
     service: imageService({
       placeholder: "blurhash",
       layout: "constrained"
     })
   },
-  adapter: node({ mode: "standalone" }),
-  server: { host: true },
+  adapter: vercel(),
   vite: {
     ssr: {
       noExternal: ["transition-style"]
